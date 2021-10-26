5e8e351cdc6123fe7b0bebc5839ed17f70d0652b
elmar8287 <L-mar@inbox.ru>
2021-10-26 17:08:23 +0400

    Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)

--- queries.sql
+++ queries.sql
@@ -5,3 +5,4 @@ SELECT date_of_birth FROM animals WHERE animal_name='Agumon' OR animal_name='Pik
 SELECT animal_name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered='1';
 SELECT * FROM animals WHERE animal_name <> 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= '10.4' AND weight_kg <= '17.3';
