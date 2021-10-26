a71a691366ee8d5c47a67c58dfe7ced7741f58d1
elmar8287 <L-mar@inbox.ru>
2021-10-26 16:22:26 +0400

    Create schema.sql file

--- schema.sql
+++ schema.sql
@@ -1,5 +1,8 @@
-/* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
-    name varchar(100)
+    id BIGSERIAL PRIMARY KEY,
+    animal_name VARCHAR(50),
+	date_of_birth DATE,
+	escape_attempts INT,
+	neutered BIT,
+	weight_kg DECIMAL
 );
