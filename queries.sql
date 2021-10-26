07e5f06a024c97383acde907e45eebf7e4495409
elmar8287 <L-mar@inbox.ru>
2021-10-26 17:01:10 +0400

    Find all animals that are neutered.

--- queries.sql
+++ queries.sql
@@ -3,3 +3,4 @@ SELECT animal_name FROM animals WHERE date_of_birth BETWEEN '2016.01.01' AND '20
 SELECT animal_name FROM animals WHERE neutered='1' AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE animal_name='Agumon' OR animal_name='Pikachu';
 SELECT animal_name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered='1';
