f45af4656b2f5ee9833c6cfb26dad2d71e4ab83d
elmar8287 <L-mar@inbox.ru>
2021-10-26 16:40:13 +0400

    Find all animals whose name ends in mon.

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE animals (
-    id BIGSERIAL PRIMARY KEY,
-    animal_name VARCHAR(50),
+  id BIGSERIAL PRIMARY KEY,
+  animal_name VARCHAR(50),
 	date_of_birth DATE,
 	escape_attempts INT,
 	neutered BIT,
