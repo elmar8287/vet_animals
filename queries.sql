9f620a7d143ed0ec6c591e8483129936faac166d
elmar8287 <L-mar@inbox.ru>
2021-10-27 17:05:19 +0400

    How many animals are there?

--- queries.sql
+++ queries.sql
@@ -35,3 +35,9 @@ SAVEPOINT SAVEPOINT_1;
 UPDATE animals
 SET weight_kg= weight_kg * '-1';
 ROLLBACK TO SAVEPOINT_1;
+UPDATE animals
+SET weight_kg= weight_kg * '-1'
+WHERE weight_kg < 0;
+COMMIT;
+
+SELECT FROM animals;
