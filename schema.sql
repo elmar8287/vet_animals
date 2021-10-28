53d67c81e8aea3ffffcdd76736d8b0ec22a645c6
elmar8287 <L-mar@inbox.ru>
2021-10-28 09:27:11 +0400

    Add owners and species tables

--- schema.sql
+++ schema.sql
@@ -1,8 +1,19 @@
 CREATE TABLE animals (
   id BIGSERIAL PRIMARY KEY,
-  animal_name VARCHAR(50),
+  animal_name VARCHAR(150),
   date_of_birth DATE,
   escape_attempts INT,
   neutered BIT,
   weight_kg DECIMAL
+);
+
+CREATE TABLE owners (
+  id BIGSERIAL PRIMARY KEY,
+  full_name VARCHAR(150),
+  age INT
+);
+
+CREATE TABLE species (
+  id BIGSERIAL PRIMARY KEY,
+  species_name VARCHAR(150)
 );
