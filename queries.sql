c623a5905e275bd47c845fdd6fe3b6cf5251ff32
elmar8287 <L-mar@inbox.ru>
2021-10-28 17:14:06 +0400

    List all Digimon owned by Jennifer Orwell.

--- queries.sql
+++ queries.sql
@@ -67,3 +67,7 @@ WHERE animals.owners_id IS NOT NULL AND (animals.owners_id IS NULL);
 SELECT species.species_name, COUNT(animals.animal_name) FROM animals
 JOIN species ON animals.species_id = species.species_id
 GROUP BY species.species_name;
+
+SELECT animals.animal_name FROM animals
+JOIN species ON animals.species_id = species.species_id
+WHERE species.species_name = 'Digimon' AND animals.owners_id = 2;
